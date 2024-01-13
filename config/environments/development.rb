b1f0b591a0a18b2eed7bd0df9bf81d4d477e0109
Brad Feehan <git@bradfeehan.com>
2024-01-14 00:21:23 +1100

    Get Web Console working in Docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow Web Console functionality from Docker host
+  config.web_console.whitelisted_ips = ENV['DOCKER_HOST_IP'] if ENV['DOCKER_HOST_IP'].present?
 end
