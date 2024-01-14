68da2ee8771996f1689a1a3343d6cd3b2d562ed2
Brad Feehan <git@bradfeehan.com>
2024-01-15 02:02:23 +1100

    Create Queries::Overpass

---
+++ app/models/queries/overpass.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Queries
+  # Represents a query of the Overpass API
+  class Overpass < Query
+  end
+end
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# Represents a query of some external API
 class Query < ApplicationRecord
+  validates :type, :name, :body, presence: true
+
+  def self.types
+    [
+      ::Queries::Overpass,
+    ].freeze
+  end
 end
