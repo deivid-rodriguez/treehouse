1bc499b10297454f8983f47fbd2c3dfd6daa5edf
Brad Feehan <git@bradfeehan.com>
2024-01-26 17:50:55 +1100

    bin/tapioca require

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,15 @@
-# typed: strict
+# typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
+require 'action_cable/engine'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'active_support/core_ext/integer/time'
+require 'bootsnap/setup'
+require 'bundler/setup'
+require 'concurrent-ruby'
+require 'rails'
