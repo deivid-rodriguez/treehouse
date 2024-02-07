e1f49ee4775d93d1a81dcaea1d43c8b6d50353af
Brad Feehan <git@bradfeehan.com>
2024-02-07 22:49:42 +1100

    Tolerate missing Domain API key at startup

--- lib/api_client/domain.rb
+++ lib/api_client/domain.rb
@@ -11,19 +11,10 @@ module APIClient
     Query = T.type_alias { T::Hash[String, T.untyped] }
 
     BASE_URL = 'https://api.domain.com.au/v1/'
-    API_KEY = T.let(ENV.fetch('DOMAIN_API_KEY'), String)
-    DEFAULT_HEADERS = T.let(
-      {
-        'Content-Type' => 'application/json',
-        'Transfer-Encoding' => 'chunked',
-        'X-API-Key' => API_KEY,
-      }.freeze,
-      T::Hash[String, String],
-    )
 
     sig { void }
     def initialize
-      connection = Faraday.new(url: BASE_URL, headers: DEFAULT_HEADERS) do |faraday|
+      connection = Faraday.new(url: BASE_URL, headers:) do |faraday|
         faraday.request :json
         faraday.response :json
         faraday.response :raise_error
@@ -65,6 +56,25 @@ module APIClient
     sig { params(block: T.proc.returns(T.untyped)).void }
     def debug_log(&block)
       Rails.logger.debug { "[#{self.class.name}]: #{block.call}" }
+    end
+
+    sig { returns(T::Hash[String, String]) }
+    def headers
+      T.let(
+        {
+          'Content-Type' => 'application/json',
+          'Transfer-Encoding' => 'chunked',
+          'X-API-Key' => api_key,
+        }.freeze,
+        T::Hash[String, String],
+      )
+    end
+
+    sig { returns(String) }
+    def api_key
+      @api_key ||= T.let(ENV.fetch('DOMAIN_API_KEY'), T.nilable(String))
+    ensure
+      raise 'No Domain API key found' if @api_key.blank?
     end
   end
 end
