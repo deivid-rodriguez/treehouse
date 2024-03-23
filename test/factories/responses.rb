d7538f05bc3675330caf2aa169b4667fcce82e18
Brad Feehan <git@bradfeehan.com>
2024-03-23 23:55:30 +1100

    fixup! Add ResponseTest

--- test/factories/responses.rb
+++ test/factories/responses.rb
@@ -7,29 +7,18 @@ FactoryBot.define do
   factory :response do
     trait :domain do
       type { Responses::DomainResponse }
-      transient do
-        query_type { :domain }
-      end
+      query factory: %i[query domain]
     end
 
     trait :overpass do
       type { Responses::OverpassResponse }
-      transient do
-        query_type { :overpass }
-      end
+      query factory: %i[query overpass]
     end
-
-    query { association(:query, query_type) }
 
     trait :fetched do
       request_body { query.body }
       body { Faker::Lorem.paragraph }
       retrieved_at { Time.current }
-    end
-
-    transient do
-      # Fallback value for query_type -- raises if neither trait is specified
-      query_type { raise 'response factory requires one of the following traits: domain, overpass' }
     end
   end
 end
